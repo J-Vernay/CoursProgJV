d65a9106d5a3a85e2cd9db4336f5828b1e96482c
pharreller <phafoubastien@gmail.com>
2025-02-12 14:49:37 +0100

    1.a

--- src/dino/dino_game.cpp
+++ src/dino/dino_game.cpp
@@ -10,17 +10,19 @@
 double lastTime = 0;
 double rotation = 360.0;
 double scale = 1.0;
-DinoVec2 circlePos = {};
+//DinoVec2 circlePos = {};
+DinoVec2 playerPos = {};
 std::vector<DinoVec2> polyline;
+bool goingRight;
 
 // Constantes.
-constexpr float CIRCLE_SPEED = 300.f; // Nombre de pixels parcourus en une seconde.
+constexpr float Dino_SPEED = 300.f; // Nombre de pixels parcourus en une seconde.
 
 void Dino_GameInit()
 {
     DinoVec2 windowSize = XDino_GetWindowSize();
     XDino_SetRenderSize(windowSize);
-    circlePos = {windowSize.x / 2, windowSize.y / 2};
+    playerPos = {windowSize.x / 2, windowSize.y / 2};
 
     polyline.emplace_back(windowSize.x * 0.2f, windowSize.y * 0.25f);
     polyline.emplace_back(windowSize.x * 0.6f, windowSize.y * 0.25f);
@@ -44,17 +46,22 @@ void Dino_GameFrame(double timeSinceStart)
         if (!bSuccess)
             continue;
 
-        if (gamepad.btn_down && !gamepad.btn_up)
+        /*if (gamepad.btn_down && !gamepad.btn_up)
             scale /= 1.01;
         if (gamepad.btn_up && !gamepad.btn_down)
             scale *= 1.01;
         if (gamepad.btn_left && !gamepad.btn_right)
             rotation += 90.0 * deltaTime;
         if (gamepad.btn_right && !gamepad.btn_left)
-            rotation -= 90.0 * deltaTime;
+            rotation -= 90.0 * deltaTime;*/
 
-        circlePos.x += gamepad.stick_left_x * CIRCLE_SPEED * deltaTime;
-        circlePos.y += gamepad.stick_left_y * CIRCLE_SPEED * deltaTime;
+        float speed = Dino_SPEED;
+        if (gamepad.btn_right)
+            speed *= 2;
+
+        playerPos.x += gamepad.stick_left_x * speed * deltaTime;
+        playerPos.y += gamepad.stick_left_y * speed * deltaTime;
+        goingRight = gamepad.stick_left_x >= 0;
     }
 
     // Affichage
@@ -76,7 +83,7 @@ void Dino_GameFrame(double timeSinceStart)
     XDino_SetRenderSize(windowSize);
 
     // Dessin de la texture centrale qu'on peut bouger.
-    {
+    /*{
         constexpr DinoColor PURPLE{0x7F, 0x58, 0xAF, 0xFF};
         constexpr DinoColor CYAN{0x64, 0xC5, 0xEB, 0xFF};
         constexpr DinoColor PINK{0xE8, 0x4D, 0x8A, 0xFF};
@@ -125,6 +132,32 @@ void Dino_GameFrame(double timeSinceStart)
     {
         DinoDrawCall drawCall = Dino_CreateDrawCall_Circle(20);
         drawCall.translation = circlePos;
+        XDino_Draw(drawCall);
+    }*/
+
+    // Dessin du dino
+    {
+        DinoDrawCall drawCall;
+        float radiusX = 24;
+        float radiusY = 24;
+        drawCall.textureName = "dinosaurs.png";
+        drawCall.vertices.reserve(6);
+        if (!goingRight)
+            radiusX = -radiusX;
+
+        DinoVec2 posA = {-radiusX, -radiusY};
+        DinoVec2 posB = {radiusX, -radiusY};
+        DinoVec2 posC = {-radiusX, radiusY};
+        DinoVec2 posD = {radiusX, radiusY};
+        DinoColor color = DinoColor_WHITE;
+
+        drawCall.vertices.emplace_back(posA, 0, 0, color);
+        drawCall.vertices.emplace_back(posB, 24, 0, color);
+        drawCall.vertices.emplace_back(posC, 0, 24, color);
+        drawCall.vertices.emplace_back(posB, 24, 0, color);
+        drawCall.vertices.emplace_back(posC, 0, 24, color);
+        drawCall.vertices.emplace_back(posD, 24, 24, color);
+        drawCall.translation = playerPos;
         XDino_Draw(drawCall);
     }
 
