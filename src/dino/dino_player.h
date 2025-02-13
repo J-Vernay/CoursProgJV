5da1382db2c5664ef5750caa7ad65266408e985b
Julien VERNAY <dev@jvernay.fr>
2025-02-13 14:47:51 +0100

    Documentation

--- src/dino/dino_player.h
+++ src/dino/dino_player.h
@@ -1,6 +1,7 @@
 
 #include <dino/xdino.h>
 
+/// Représente l'état d'un joueur.
 class DinoPlayer {
 private:
     DinoVec2 pos = {};
@@ -13,9 +14,15 @@ private:
     int m_idxPlayer = 0;
     DinoGamepadIdx m_idxGamepad;
 public:
+    /// Configure le joueur pour répondre à une manette en particulier.
     void Init(DinoVec2 initPos, int32_t idxPlayer, DinoGamepadIdx idxGamepad);
+
+    /// Met à jour la position du joueur et sa logique d'animation.
     void UpdatePlayer(float deltaTime);
+
+    /// Affiche le joueur à l'écran.
     void DrawPlayer(double timeSinceStart);
 
+    /// Compare la position de deux joueurs pour savoir lequel afficher devant l'autre.
     bool IsAbove(DinoPlayer& other);
 };
