324b024c935e2074b702cf3512c1209ccbddb0b6
Geek_Joystick <contact.geekjoystick@gmail.com>
2025-02-13 11:27:12 +0100

    4.g

--- src/dino/dino_terrain.cpp
+++ src/dino/dino_terrain.cpp
@@ -5,12 +5,22 @@
 constexpr int TERRAIN_WIDTH_TOTAL = TERRAIN_WIDTH + 2;
 constexpr int TERRAIN_HEIGHT_TOTAL = TERRAIN_HEIGHT + 2;
 constexpr float ANIMATION_FRAMERATE = 4;
+
 const int terrainAnimationFrames[] = {
     0, 1, 2, 1, 0
 };
 
+const DinoColor oceanColors[] = {
+    {0, 146, 221, 255},
+    {0, 146, 221, 255},
+    {0, 146, 221, 255},
+    {153, 207, 242, 255}
+};
+
 void DinoTerrain::init()
 {
+    season = static_cast<Season>(XDino_RandomInt32(0, Season::WINTER));
+    
     for (int y = 0; y < TERRAIN_HEIGHT; y++) {
         for (int x = 0; x < TERRAIN_WIDTH; x++) {
             int16_t tile = XDino_RandomInt32(0, 20);
@@ -34,17 +44,16 @@ void DinoTerrain::update(float deltaTime)
 
 void DinoTerrain::draw_ocean()
 {
-    constexpr DinoColor CLEAR_COLOR = {0, 146, 221, 255};
-    XDino_SetClearColor(CLEAR_COLOR);
+    XDino_SetClearColor(oceanColors[season]);
 }
 
 
 void DinoTerrain::draw_terrain()
 {
     DinoVec2 renderSize = XDino_GetRenderSize();
-
     DinoVec2 offset = {(renderSize.x - TERRAIN_WIDTH_TOTAL * 16) / 2.0f,
                        (renderSize.y - TERRAIN_HEIGHT_TOTAL * 16) / 2.0f};
+    uint16_t uSeason = season * 5;
 
     for (int x = 0; x < TERRAIN_WIDTH_TOTAL; x++) {
         int16_t u = 1;
@@ -56,31 +65,31 @@ void DinoTerrain::draw_terrain()
             u = 2;
         }
 
-        DinoDrawCall drawCall = Dino_CreateDrawCall_Sprite(u, 1 + terrainAnimationFrames[frame] * 3, 16, 16);
+        DinoDrawCall drawCall = Dino_CreateDrawCall_Sprite(u + uSeason, 1 + terrainAnimationFrames[frame] * 3, 16, 16);
         drawCall.textureName = "terrain.png";
         drawCall.translation = {offset.x + x * 16, offset.y};
         XDino_Draw(drawCall);
 
-        drawCall = Dino_CreateDrawCall_Sprite(u, 3 + terrainAnimationFrames[frame] * 3, 16, 16);
+        drawCall = Dino_CreateDrawCall_Sprite(u + uSeason, 3 + terrainAnimationFrames[frame] * 3, 16, 16);
         drawCall.textureName = "terrain.png";
         drawCall.translation = {offset.x + x * 16, offset.y + 13 * 16};
         XDino_Draw(drawCall);
     }
 
     for (int y = 0; y < TERRAIN_HEIGHT; y++) {
-        DinoDrawCall drawCall = Dino_CreateDrawCall_Sprite(0, 2 + terrainAnimationFrames[frame] * 3, 16, 16);
+        DinoDrawCall drawCall = Dino_CreateDrawCall_Sprite(uSeason, 2 + terrainAnimationFrames[frame] * 3, 16, 16);
         drawCall.textureName = "terrain.png";
         drawCall.translation = {offset.x, offset.y + (y + 1) * 16};
         XDino_Draw(drawCall);
 
-        drawCall = Dino_CreateDrawCall_Sprite(2, 2 + terrainAnimationFrames[frame] * 3, 16, 16);
+        drawCall = Dino_CreateDrawCall_Sprite(2 + uSeason, 2 + terrainAnimationFrames[frame] * 3, 16, 16);
         drawCall.textureName = "terrain.png";
         drawCall.translation = {offset.x + (TERRAIN_WIDTH_TOTAL - 1) * 16, offset.y + (y + 1) * 16};
         XDino_Draw(drawCall);
     }
 
     {
-        DinoDrawCall drawCall = Dino_CreateDrawCall_Sprite(1, 0, 16, 16);
+        DinoDrawCall drawCall = Dino_CreateDrawCall_Sprite(1 + uSeason, 0, 16, 16);
 
         drawCall.vertices[1].pos.y = TERRAIN_HEIGHT * 16;
         drawCall.vertices[2].pos = {TERRAIN_WIDTH * 16, TERRAIN_HEIGHT * 16};
@@ -96,7 +105,7 @@ void DinoTerrain::draw_terrain()
         for (int x = 0; x < TERRAIN_WIDTH; x++) {
             if (decorations[y * TERRAIN_WIDTH + x] == 0) continue;
             
-            DinoDrawCall drawCall = Dino_CreateDrawCall_Sprite(decorations[y * TERRAIN_WIDTH + x], 0, 16, 16);
+            DinoDrawCall drawCall = Dino_CreateDrawCall_Sprite(decorations[y * TERRAIN_WIDTH + x] + uSeason, 0, 16, 16);
             drawCall.textureName = "terrain.png";
             drawCall.translation = {offset.x + (x + 1) * 16, offset.y + (y + 1) * 16};
             XDino_Draw(drawCall);
--- src/dino/dino_terrain.h
+++ src/dino/dino_terrain.h
@@ -4,8 +4,18 @@ constexpr int TERRAIN_WIDTH = 16;
 constexpr int TERRAIN_HEIGHT = 12;
 
 class DinoTerrain {
+public:
+    enum Season {
+        SPRING,
+        SUMMER,
+        AUTUMN,
+        WINTER
+    };
+    
+private:
     float timer = 0;
     int frame = 0;
+    Season season = SPRING;
 
     int16_t decorations[TERRAIN_WIDTH * TERRAIN_HEIGHT];
 
