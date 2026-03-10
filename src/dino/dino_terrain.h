f34d04771ffb97cf4a2c3f13efe80fef20e1c6a6
Julien VERNAY <dev@jvernay.fr>
2026-03-10 17:07:08 +0100

    3f) / F2.3

--- src/dino/dino_terrain.h
+++ src/dino/dino_terrain.h
@@ -11,4 +11,7 @@ public:
 private:
     uint64_t m_vbufID = 0;
     uint64_t m_texID = 0;
+
+    void GenFlowers(std::vector<DinoVertex>& out, int idxSeason, DinoVec2 posTopLeft);
+    void GenFlower(std::vector<DinoVertex>& out, int idxSeason, int idxFlower, DinoVec2 pos);
 };
