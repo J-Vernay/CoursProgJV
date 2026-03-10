34139a8aed9bd9046df0a332653a366e70fee45a
Abal <bergerbarnabe@gmail.com>
2026-03-10 15:24:42 +0100

    dino terrain class creation

---
+++ src/dino/dino_terrain.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <dino/xdino.h>
+
+class DinoTerrain {
+public:
+    void Init(DinoVec2 rdrSize);
+    void Draw();
+    void Shut();
+
+private:
+    uint64_t m_vbufID = 0;
+    uint64_t m_texID = 0;
+};
