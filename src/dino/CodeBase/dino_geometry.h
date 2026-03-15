43208c3d47a6c9e91b1e4ff4a117d2e18a9509b2
Aspaross <159767272+Aspaross@users.noreply.github.com>
2026-03-15 19:09:32 +0100

    y

--- src/dino/CodeBase/dino_geometry.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <dino/xdino.h>
-
-/// Vérifie si les deux segments [AB] et [CD] ont une intersection.
-bool Dino_IntersectSegment(DinoVec2 a, DinoVec2 b, DinoVec2 c, DinoVec2 d);
